ee852d41154f344c577e3c50cfc0d80d75d0f961
sworc <lightbox@nifty.com>
2022-04-28 17:02:40 +0900

    Update comp.bat

--- tomcat/comp.bat
+++ tomcat/comp.bat
@@ -1,1 +1,1 @@
-\java\bin\javac -encoding utf8 -classpath "..;\xampp\tomcat\lib\servlet-api.jar;\xampp\tomcat\lib\jsp-api.jar" Sample1.java
+\java16\bin\javac -encoding utf8 -classpath "..;\xampp\tomcat\lib\servlet-api.jar;\xampp\tomcat\lib\jsp-api.jar" Sample1.java
